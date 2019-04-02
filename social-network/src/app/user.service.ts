84d9453d8ed5a86a9008a2ac070ffa6e09693dd6
RudolfKlikh <rudolfklikh@gmail.com>
2019-04-02 03:46:40 +0200

    Creating a service for http request,receiving data and displaying the first card

---
+++ social-network/src/app/user.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators/';
+
+
+
+@Injectable()
+export class UserService {
+   constructor(private http: HttpClient) {}
+
+    private getUsers()  {
+        return this.http.get(`https://randomuser.me/api/?inc=gender,name,picture,location&results=1&nat=gb`)
+        .pipe(map(response => response.results ))
+        .pipe(map(users => {
+          return users.map(user => {
+            return {
+              name : `${user.name.first} ${user.name.last}`,
+              image: `${user.picture.medium}`,
+              geo : `${user.location.city} ${user.location.state} ${user.location.street}`
+            };
+          })
+       }))
+    } 
+    public GetgetUsers() {
+        return this.getUsers();
+    }
+}
