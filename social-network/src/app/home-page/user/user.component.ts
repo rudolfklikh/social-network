1e1c2eac7de8e858302c2b9538062cefe1a2c613
RudolfKlikh <rudolfklikh@gmail.com>
2019-05-25 19:52:51 +0200

    Set / Get in Input() , :host pseudo-element

--- social-network/src/app/home-page/user/user.component.ts
+++ social-network/src/app/home-page/user/user.component.ts
@@ -4,13 +4,23 @@ import { User } from '../../user';
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  inputs : ['user']
 })
 export class UserComponent implements OnInit {
+
+
+  private _user : User ;
   constructor() { 
 
   }
 
+  @Input() set user(user : User) {
+    this._user = user;
+  }
+  
+  
+  get user() : User {
+    return this._user;
+  } 
   ngOnInit() {
   }
 
